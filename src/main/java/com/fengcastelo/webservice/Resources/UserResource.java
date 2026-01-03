1bed52bc06cf182a511e1151722420b6f3865e09
FengCastelo <ianfcastelo7@gmail.com>
2026-01-03 17:37:21 -0300

    User DTO created

--- src/main/java/com/fengcastelo/webservice/Resources/UserResource.java
+++ src/main/java/com/fengcastelo/webservice/Resources/UserResource.java
@@ -2,6 +2,7 @@ package com.fengcastelo.webservice.Resources;
 
 import com.fengcastelo.webservice.Model.User;
 import com.fengcastelo.webservice.Services.UserService;
+import com.fengcastelo.webservice.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -18,8 +20,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDto);
     }
 }
